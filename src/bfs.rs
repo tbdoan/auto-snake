5bddd6a0ce601b9b0a6c50bfc4396724ee50103c
tung <->
2026-08-08 21:52:07 -0700

    impl bfs

---
+++ src/bfs.rs
@@ -0,0 +1,65 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+use crate::arena::Coord;
+use crate::arena::Dimensions;
+use crate::arena::MoveDirection;
+
+/// Return path, inclusive start and end. If no path found, return empty
+pub fn bfs(start: Coord, end: Coord, dim: Dimensions, obstacles: &[Coord]) -> Vec<Coord> {
+    assert!(start != end, "start must not equal end");
+    assert!(!obstacles.contains(&end), "end is guaranteed open");
+
+    // start at the snake's head
+    let mut q = VecDeque::new();
+    let mut v = HashSet::new();
+    q.push_back(start);
+    v.insert(start);
+
+    // add "obstacles" to the visited set, so we dont traverse
+    v.extend(obstacles);
+
+    // for path reconstruction
+    let mut child_parent = HashMap::new();
+
+    'bfs: while let Some(cur) = q.pop_front() {
+        for dir in [
+            MoveDirection::Up,
+            MoveDirection::Down,
+            MoveDirection::Left,
+            MoveDirection::Right,
+        ] {
+            let nxt = cur.move_in(dir);
+            if dim.check_oob(nxt) {
+                continue;
+            }
+            if v.contains(&nxt) {
+                continue;
+            }
+            child_parent.insert(nxt, cur);
+            v.insert(nxt);
+            q.push_back(nxt);
+
+            // we want to make sure the target ends up in the mapping
+            if nxt == end {
+                break 'bfs;
+            }
+        }
+    }
+
+    if !child_parent.contains_key(&end) {
+        return Vec::new();
+    }
+
+    // reconstruct the path
+    let mut path = Vec::new();
+    let mut cur = &end;
+    while let Some(parent) = child_parent.get(cur) {
+        path.push(*cur);
+        cur = parent;
+    }
+    path.push(*cur);
+    path.reverse();
+    return path;
+}
